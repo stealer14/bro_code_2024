1f65d6920d07329fddec51f8acc35ee3cb3605b9
Nestor Ramirez <stealer14@hotmail.com>
2024-12-03 13:24:26 -0600

    Update 1-variables.py

--- 1-variables.py
+++ 1-variables.py
@@ -1,1 +1,18 @@
 #Chapter 1 
+# Variable = A container for a value (string, integer, float, bolean)
+
+# Strings
+first_name = "Bro"
+food = "pizza"
+email = "bro123@fake.com
+
+# Integers
+age = 25
+quantity = 3
+num_students = 30
+
+# Float
+price = 10.99
+
+print(f"The price is {price})
+
