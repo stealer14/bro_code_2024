2bd120af4c6b9669329d0cd11d4dcb0db27247eb
stealer14 <stealer14@hotmail.com>
2025-03-30 10:51:46 -0500

    Create separate folder for basic programming and OOP

--- python/4-input.py
+++ python/4-input.py
@@ -7,8 +7,9 @@ name = input("What is your name?: ")
 age = input("How old are you?: ")
 
 age = int(age)
-age  = age + 1
+age_next  = age + 1
 
 print(f"Hello, {name}")
 print("Happy Birthday!")
-print(f"You are {age} years old")
+print(f"You are {age} years old today")
+print(f"Next year you'll be {age_next} years old ")
---
+++ python/4.3-madlib.py
@@ -0,0 +1,18 @@
+import pandas as pd
+# Madlibs game
+# word game where you create a story
+# by filling in blanks with random words
+
+adjective1 = input("Enter an adjective (dewcription): ")
+noun1 = input("Enteer a nount person, place, thing): ")
+adjective2 = input("Enter an adjective (description): ")
+verb1 = input("Enter a verb ending with 'ing'")
+adjective3 = input("Enter an adjective (description): ")
+
+print("\n")
+print("==============This is your story===============")
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
+
